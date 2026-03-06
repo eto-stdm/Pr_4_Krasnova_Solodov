698f91f496c0cbd1582ce3f4ddc44519e6a458d6
uwu <potatocuteuwu@gmail.com>
2026-03-06 13:16:27 +0300

    переделка функции подсчёта на первой странице

--- UnitTestProject/UnitTest.cs
+++ UnitTestProject/UnitTest.cs
@@ -11,14 +11,35 @@ namespace UnitTestProject
         public void Page1_1()
         {
             var page = new Page1();
-            Assert.IsTrue(page.IsAbleToCalculate("1", "2", "4"));
+            Assert.IsTrue(page.Calculate("1", "2", "4"));
         }
         [TestMethod]
         public void Page1_2()
         {
             var page = new Page1();
-            Assert.IsFalse(page.IsAbleToCalculate("wewedw", "2", "4"));
+            Assert.IsFalse(page.Calculate("wewedw", "2", "4"));
         }
+        [TestMethod]
+
+        public void Page2_1()
+        {
+            var page = new Page3();
+            Assert.IsFalse(page.IsAbleToCalculate("32"));
+        }
+
+        [TestMethod]
+        public void Page2_2()
+        {
+            var page = new Page3();
+            Assert.IsFalse(page.IsAbleToCalculate("32"));
+        }
+        [TestMethod]
+        public void Page2_3()
+        {
+            var page = new Page3();
+            Assert.IsFalse(page.IsAbleToCalculate("32"));
+        }
+
         [TestMethod]
         public void Page3_1()
         {
